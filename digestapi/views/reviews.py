4ecf8d2e5e9000520fdcadf40c43b2ef643e840b
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-26 09:10:40 -0500

    delete review, create review

--- digestapi/views/reviews.py
+++ digestapi/views/reviews.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, status, serializers, permissions
 from rest_framework.response import Response
-from digestapi.models import Review
+from digestapi.models import Review, Book
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -34,20 +34,25 @@ class ReviewViewSet(viewsets.ViewSet):
         review = Review()
 
         # values from the request payload using `request.data`
+        chosen_book = Book.objects.get(pk=request.data["book"])
+
         review.rating = request.data["rating"]
         review.comment = request.data["comment"]
-        review.date = request.data["date"]
+        # review.date = request.data["date"]
         review.user = request.auth.user
-        review.book = request.data["book"]
+        review.book = chosen_book
 
         # Save the review
         review.save()
 
         try:
-            return "hello"
             # Serialize the objects, and pass request as context
+            serialized = ReviewSerializer(
+                review, many=False, context={"request": request}
+            )
 
             # Return the serialized data with 201 status code
+            return Response(serialized.data, status=status.HTTP_201_CREATED)
 
         except Exception as ex:
             return Response(None, status=status.HTTP_400_BAD_REQUEST)
@@ -72,15 +77,19 @@ class ReviewViewSet(viewsets.ViewSet):
             review = Review.objects.get(pk=pk)
 
             # Check if the user has permission to delete
+            if review.user.id == request.user.id:
+                review.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
             # Will return 403 if authenticated user is not author
-            if review.user.id != request.user.id:
+            else:
+                review.user.id != request.user.id
                 return Response(status=status.HTTP_403_FORBIDDEN)
 
-            # Delete the review
-            review.delete()
+            # # Delete the review
+            # review.delete()
 
-            # Return success but no body
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            # # Return success but no body
+            # return Response(status=status.HTTP_204_NO_CONTENT)
 
         except Review.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
